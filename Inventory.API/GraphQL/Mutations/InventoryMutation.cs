8cb3e99b2955cd59c9a1e7266a918160fbf74d04
Bruno Di Giuseppe <bruno@ascentsoftware.eu>
2020-05-24 15:33:18 +0200

    added CreateItem Mutation

---
+++ Inventory.API/GraphQL/Mutations/InventoryMutation.cs
@@ -0,0 +1,29 @@
+﻿using API_Inventory.GraphQL.Types;
+using Domain_Inventory.Infra;
+using Domain_Inventory.Model;
+using GraphQL;
+using GraphQL.Types;
+using Inventory.API.GraphQL.Types.Mutations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory.API.GraphQL.Mutations
+{
+    public class InventoryMutation : ObjectGraphType
+    {
+        public InventoryMutation(IItemRepo itemRepo)
+        {
+            FieldAsync<ItemType>(
+                "createItem",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<ItemInputType>> { Name = "item" }),
+                resolve: async context =>
+                {
+                    var item = context.GetArgument<Item>("item");
+                    return await context.TryAsyncResolve(async c => (await itemRepo.Add(new List<Item> { item })).FirstOrDefault() );
+                }
+            );
+        }
+    }
+}
---
+++ Inventory.API/GraphQL/Types/Mutations/MovementInputType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory.API.GraphQL.Types.Mutations
+{
+    public class MovementInputType : InputObjectGraphType
+    {
+        public MovementInputType()
+        {
+            Name = "movementInput";
+            Field<NonNullGraphType<IntGraphType>>("quantity");
+            Field<StringGraphType>("description");
+        }
+    }
+}
